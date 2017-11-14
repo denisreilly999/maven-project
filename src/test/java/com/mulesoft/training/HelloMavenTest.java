89626e9ea1170883692310368e4de6fc1095e320
denis.reilly <denis.reilly@graydon.co.uk>
2017-11-14 08:26:26 +0000

    fix

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -12,7 +12,7 @@ public class HelloMavenTest extends FunctionalTestCase {
     
     @Override
     protected String getConfigFile() {
-        return "maven-project.xml";
+        return "module0.xml";
     }
 
 }
